bd9d490c148789034668b7ad0ed944ffa645b67e
hoseinmohajer <hoseinmohajer@gmail.com>
2020-07-27 23:22:31 +0430

    refactor treeMenu to work with checked=bool instead of ids

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import TreeMenu from './components/treeMenu'
 const menuItems = [
   {
     id: 1,
+    checked: true,
     title: 'level one',
     children: [],
   },
   {
     id: 2,
+    checked: false,
     title: 'level one',
     children: [
       {
         id: 21,
+        checked: false,
         title: 'level two',
         children: [],
       },
@@ -20,32 +23,58 @@ const menuItems = [
   },
   {
     id: 3,
+    checked: false,
     title: 'level one',
     children: [
       {
         id: 31,
+        checked: false,
         title: 'level two',
         children: [
           {
             id: 311,
+            checked: false,
             title: 'level three',
             children: [
               {
                 id: 3111,
+                checked: false,
                 title: 'level four',
                 children: [],
               },
               {
                 id: 3112,
+                checked: false,
                 title: 'level four',
                 children: [
                   {
                     id: 31121,
+                    checked: false,
                     title: 'level five',
                     children: [],
                   },
                 ],
               },
+              {
+                id: 3113,
+                checked: false,
+                title: 'level four',
+                children: [
+                  {
+                    id: 31131,
+                    checked: false,
+                    title: 'level four',
+                    children: []
+                  }
+                ],
+              },
+              {
+                id: 3114,
+                checked: false,
+                title: 'level four',
+                children: [],
+              },
+
             ],
           },
         ],
@@ -57,7 +86,7 @@ const menuItems = [
 function App() {
   return (
     <TreeMenu
-      menuItems={menuItems}
+      data={menuItems}
       iconColor='gray'
       fontColor='black'
       hasLabelIcon
--- src/components/treeMenu/index.js
+++ src/components/treeMenu/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { MenuTreeWrapper, LabelIcon, TreeMenuTitle } from './style';
 
-export default ({menuItems, labelIcon, leftSideLabel, iconColor, fontColor, hasLabelIcon}) => {
+export default ({data, labelIcon, leftSideLabel, iconColor, fontColor, hasLabelIcon}) => {
+  const [menuItems, setMenuItems] = useState(data);
   const [hiddenIds, setHiddenIds] = useState([]);
-  const [checkedIds, setCheckedIds] = useState([]);
   const toggle = (id) => {
     if (hiddenIds.indexOf(id) !== -1) {
       setHiddenIds(hiddenIds.filter((hiddenId) => hiddenId !== id));
@@ -21,25 +21,50 @@ export default ({menuItems, labelIcon, leftSideLabel, iconColor, fontColor, hasL
     });
     return IDs;
   };
-  const onChangeHandler = (id, children) => {
-    let childrenIds = [];
-    if (children.length !== 0) {
-      IDs = [];
-      childrenIds = getChildrenId(children);
-    }
-
-    if (checkedIds.indexOf(id) !== -1) {
-      setCheckedIds(checkedIds.filter((hiddenId) => hiddenId !== id));
+  const onChangeHandler = (id, status) => {
+    const items = JSON.parse(JSON.stringify(menuItems));
+    let result = [];
+    if (status) {
+      result = unChecked(items, id);
     } else {
-      const IDs = checkedIds.concat(childrenIds);
-      setCheckedIds(IDs.concat([id]));
+      result = checked(items, id);
     }
+    return setMenuItems(result);
+  };
+
+  const checked = (array, id) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id === id) {
+        array[i].checked = true;
+        if (array[i].children && array[i].children.length !== 0) {
+          for (let j = 0; j < array[i].children.length; j++) {
+            checked(array[i].children, array[i].children[j].id);
+          }
+        }
+      } else {
+        checked(array[i].children, id);
+      }
+    }
+    setMenuItems(array);
+    return array;
+  };
+  const unChecked = (array, id) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id === id) {
+        array[i].checked = false;
+      } else {
+        unChecked(array[i].children, id);
+      }
+    }
+    setMenuItems(array);
+    return array;
   };
   const menuEngine = (menuItems) => {
     return (
       <MenuTreeWrapper>
         <ul className="tree-menu-ul">
           {menuItems.map((item) => {
+            console.log(item);
             const status = item.children && item.children.length !== 0 && hiddenIds.indexOf(item.id) === -1;
             const hasChildren = item.children && item.children.length !== 0;
             return (
@@ -54,8 +79,8 @@ export default ({menuItems, labelIcon, leftSideLabel, iconColor, fontColor, hasL
                     )}
                     <input
                       type="checkbox"
-                      checked={checkedIds.indexOf(item.id) !== -1}
-                      onChange={() => onChangeHandler(item.id, item.children)}
+                      checked={(item.checked)}
+                      onChange={() => onChangeHandler(item.id, item.checked)}
                     />
                     {hasLabelIcon && <LabelIcon className={`sitemap-icon ${labelIcon || 'icon-sitemap'}`} iconColor={iconColor}/>}
                     <TreeMenuTitle fontColor={fontColor}>{item.title}</TreeMenuTitle>
